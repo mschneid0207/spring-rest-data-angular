9873e4439984284e55942be4ad594352fa79e6bf
mschneid0207 <markus.schneider12@allianz.de>
2015-11-29 21:44:06 +0100

    added spring boot - but application is not runnable

--- src/main/java/de/mschneid/entity/Task.java
+++ src/main/java/de/mschneid/entity/Task.java
@@ -11,20 +11,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Task {
-	
+
 	@Id
-	 @GeneratedValue(strategy = GenerationType.AUTO)
-	 @Column(name="ID")
-
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
 	private int id;
+	@Column(name= "task_description")
 	private String description;
+	@Column(name= "task_priority")
 	private String priority;
+	@Column(name= "task_status")
 	private String status;
+	@Column(name= "task_archived")
 	private boolean isArchived;
-	
-	
-	
-	 
-
 
 }
